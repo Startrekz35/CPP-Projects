976bfecac8eb16b6ee26d9983ef84b8507974a33
Startrekz35 <burkz3905@gmail.com>
2021-07-29 21:06:27 -0500

    FINAL VERSION

--- CPPDrill.cpp
+++ CPPDrill.cpp
@@ -9,20 +9,20 @@ int main()
     cout << "Dear, " << first_name << "\n" 
     << " How are you? I am fine. I really miss you.\n\n";
 
-    cout << "Give me another friend's name\n";
+    cout << "Give me another friend's name\n\n";
     string friend_name;
     cin >> friend_name;
-    cout << "Have you seen " << friend_name << " lately??\n";
-    char friend_sex = 0, m = 1, f = 2;
+    cout << "Have you seen " << friend_name << " lately??\n\n";
+    char friend_sex = 0, m, f;
     cout << "Input your friend's sex (type m or f then press enter)\n";
     cin >> friend_sex;
-    
-    if (friend_sex = m) 
-    cout << "If you see " << friend_name << " please ask him to call me.\n";
-    if (friend_sex = f) 
-    cout << "If you see " << friend_name << " please ask her to call me.\n";
 
-    cout << "Enter" << first_name <<"'s age\n";
+    if (friend_sex = m) 
+    cout << "If you see " << friend_name << " please ask him to call me.\n\n";
+    else if (friend_sex = f) 
+    cout << "If you see " << friend_name << " please ask her to call me.\n\n";
+
+    cout << "Enter " << first_name <<"'s age\n";
     int age = 0;
     cin >> age;
 
@@ -32,14 +32,17 @@ int main()
     if (age <= 12) 
     cout << "Next year you will be " << age+1 << ".\n";
     
-    if (age = 17) 
+    if (age == 17) 
     cout << "DRINKING AGE COMING SOON?!?!?!\n";
     
     if (age >= 70) 
     cout << "I hope you are enjoying retirement and staying active.\n";
 
-    cout << "Write your full name.\n";
-    string full_name;
-    cin >> full_name;
-    cout << "Yours sincerely, " << full_name;
+    cout << "Write your first name.\n";
+    string your_first, your_last;
+    cin >> your_first;
+    cout << "Now your last name.\n";
+    cin >> your_last;
+    string full_name = your_first + " " + your_last; 
+    cout << "Yours sincerely, " << full_name << "\n";
 }
