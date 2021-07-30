cbfa43e6655ec4ed8cb431d31c8ed1608011823f
Startrekz35 <burkz3905@gmail.com>
2021-07-29 23:18:16 -0500

    FINAL FINAL

--- CPPDrill.cpp
+++ CPPDrill.cpp
@@ -17,9 +17,9 @@ int main()
     cout << "Input your friend's sex (type m or f then press enter)\n";
     cin >> friend_sex;
 
-    if (friend_sex = m) 
+    if (friend_sex == 'm') 
     cout << "If you see " << friend_name << " please ask him to call me.\n\n";
-    else if (friend_sex = f) 
+    else if (friend_sex == 'f') 
     cout << "If you see " << friend_name << " please ask her to call me.\n\n";
 
     cout << "Enter " << first_name <<"'s age\n";
