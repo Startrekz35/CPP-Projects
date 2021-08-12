36b25cd660de6d47bf20d13b4aae2aea09fd3baa
Startrekz35 <burkz3905@gmail.com>
2021-08-12 14:05:25 -0500

    New Version

---
+++ integervaluev2.cpp
@@ -0,0 +1,29 @@
+#include "std_lib_facilities.h"
+
+int main()
+{
+    double val1, val2;
+    cout << "Enter two values\n";
+    cin >> val1 >> val2;
+    
+    if (val1 < val2) 
+    cout << val1 
+    << " is smaller than " 
+    << val2 <<'\n' 
+    << val2 << " is larger than " << val1 << "\n";
+
+    if (val1 > val2)
+    cout << val2
+    << " is smaller than " 
+    << val1 <<'\n' 
+    << val1 << " is larger than " << val2 << "\n";
+
+    if (val1 == val2)
+    cout << val1 << " and " << val2 << " are the same\n"; 
+
+    cout << "Added together is (" << val1 + val2 << ")\n";
+    cout << "Multiplied is (" << val1 * val2 << ")\n";
+    cout << "Divided is (" << val1 / val2 << " or " << val2 / val1 << ")\n";
+    cout << "Subtracted is (" << val1 - val2 << " or " << val2 - val1 << ")\n";
+
+}
