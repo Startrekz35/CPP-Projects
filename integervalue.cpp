0332253e5b4526f2ed6bc6cd207ab9b3b05d9f63
Startrekz35 <burkz3905@gmail.com>
2021-08-12 13:37:11 -0500

    Integer Value Completed

--- integervalue.cpp
+++ integervalue.cpp
@@ -3,8 +3,6 @@
 int main()
 {
     int val1, val2;
-    double val3;
-    val3 = val1 + val2;
     cout << "Enter two values\n";
     cin >> val1 >> val2;
     
@@ -12,17 +10,21 @@ int main()
     cout << val1 
     << " is smaller than " 
     << val2 <<'\n' 
-    << val2 << " is larger than " << val1;
+    << val2 << " is larger than " << val1 << "\n";
 
     if (val1 > val2)
     cout << val2
     << " is smaller than " 
     << val1 <<'\n' 
-    << val1 << " is larger than " << val2;
+    << val1 << " is larger than " << val2 << "\n";
 
-    cout << val3 << '\n' 
-    << val4 << '\n' 
-    << val5 << "\n" 
-    << val6;
+    if (val1 == val2)
+    cout << val1 << " and " << val2 << " are the same\n"; 
+
+    cout << "Added together is (" << val1 + val2 << ")\n";
+    cout << "Multiplied is (" << val1 * val2 << ")\n";
+    cout << "Divided is (" << val1 / val2 << " or " << val2 / val1 << ")\n";
+    cout << "Subtracted is (" << val1 - val2 << " or " << val2 - val1 << ")\n";
+    cout << "Modulus is (" << val1 % 2 <<  " or " << val2 % val1 << ")\n";
 
 }
